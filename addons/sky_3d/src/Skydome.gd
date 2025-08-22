d1323b625436bbf90d64e2df85fae710a2b4296c
Cory Petkovsek <632766+TokisanGames@users.noreply.github.com>
2025-08-22 22:37:58 +0700

    Update addons/sky_3d/src/Skydome.gd

--- addons/sky_3d/src/Skydome.gd
+++ addons/sky_3d/src/Skydome.gd
@@ -683,7 +683,7 @@ func atm_moon_phases_mult() -> float:
 func atm_night_intensity() -> float:
 	if not atm_enable_moon_scatter_mode:
 		return clampf(-sun_direction().y + 0.30, 0., 1.)
-	return clampf(moon_direction().y, 0, 1) * atm_moon_phases_mult()
+	return clampf(moon_direction().y, 0., 1.) * atm_moon_phases_mult()
 
 
 func fog_atm_night_intensity() -> float:
